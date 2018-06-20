3d2cf6c4b16d18876d8f9ed670fdc895f2d6da3c
LangY0628 <37542362+LangY0628@users.noreply.github.com>
2018-06-20 19:25:31 +0800

    Add files via upload

---
+++ blog/admin/contentClass/add.php
@@ -0,0 +1,40 @@
+﻿<?php
+include'../../public/common/config.php';
+session_start();
+if(@!$_SESSION['userid']){
+    echo "<script>location='../login.php'</script>";
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>index</title>
+	<link rel="stylesheet" type="text/css" href="../../public/css/bootstrap.min.css" >
+
+</head>
+<body>
+	<div class="container">
+		<h1 class="page-header">
+			<a href="" class='btn btn-primary'>添加</a>
+			<a href="index.php" class='btn btn-warning'>查看</a>
+		</h1>
+
+		<form action="insert.php" method="post">
+            <div class="form-group">
+                <label for="class_name">分类名称</label>
+                <input class="form-control" type="text" name="class_name" id="class_name">
+
+            </div>
+			<div class="form-group">
+				<input type="submit" class="btn btn-primary" value="提交">
+				<input type="reset" class="btn btn-danger">
+			</div>
+
+		</form>
+	</div>
+    <script type="text/javascript" src="../../public/js/jquery-1.12.3.min.js"></script>
+    <script type="text/javascript" src="../../public/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ blog/admin/contentClass/delete.php
@@ -0,0 +1,18 @@
+﻿<?php
+include'../../public/common/config.php';
+session_start();
+if(@!$_SESSION['userid']){
+    echo "<script>location='../login.php'</script>";
+    exit;
+}
+
+$id=$_GET['id'];
+
+$sql="delete from contentclass where id={$id}";
+
+$smt=$pdo->prepare($sql);
+
+if($smt->execute()){
+	echo "<script>location='index.php'</script>";
+}
+ ?>
